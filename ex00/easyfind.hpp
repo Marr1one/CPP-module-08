d6cea7bced6e7783f1e191d0fc12514ff53c729d
Marwan <maraisscode@gmail.com>
2025-10-29 21:51:08 +0100

    ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 23:54:55 by root              #+#    #+#             */
+/*   Updated: 2025/10/29 17:37:43 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <algorithm>
+
+
+template <typename T>
+void easyfind(T &container, int n)
+{
+	typename T::iterator it;
+	it = std::find(container.begin(), container.end(), n);
+	if (it == container.end())
+		throw std::runtime_error("not found\n");
+	std::cout << n << " founded !\n";
+}
